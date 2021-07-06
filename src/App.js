876ca953b1417b72cb45a20fbfd58e481c76ab49
andurebel <andu.trica@gmail.com>
2021-07-06 23:14:01 +0300

    5.Styled AboutSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import AboutMe from "./pages/AboutMe";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,68 @@
 import React from "react";
+//styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>I work to bring</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>message</span> to
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>the world</h2>
-          </div>
+          </Hide>
         </div>
-        <p>Contact me to bring your ideas to life</p>
+        <p>
+          Contact me to bring your ideas to life.I will make sure you are taking
+          the best decision.
+        </p>
         <button>Contact me</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img
           src="https://images.unsplash.com/photo-1614790871804-fe037bdc1214?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZGV2ZWxvcGVyfGVufDB8MXwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           alt="me"
         />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
 
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 export default AboutSection;

eb519420b30c6cdea2b79e0f697a3536ae84b665
andurebel <andu.trica@gmail.com>
2021-07-07 22:47:00 +0300

    8.Added React router+links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import AboutMe from "./pages/AboutMe";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import MyWork from "./pages/MyWork";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutMe />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutMe />
+        </Route>
+        <Route path="/work">
+          <MyWork />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
